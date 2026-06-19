9fec5efdb030e0497558ea444b2642e1a5600319
BaCh-296 <bartolomechiappe@gmail.com>
2026-06-18 23:22:59 -0300

    Actualizacion de main.c
    - Filtro promedio movil
    - Detección de latidos(BPM)

--- Presentacion_ISE/Core/Src/main.c
+++ Presentacion_ISE/Core/Src/main.c
@@ -1,37 +1,113 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
 #include "main.h"
 #include "adc.h"
 #include "usart.h"
 #include "gpio.h"
 
+/* USER CODE BEGIN Includes */
 #include <stdio.h>
+#include <stdint.h>
+/* USER CODE END Includes */
 
 void SystemClock_Config(void);
 
-uint32_t adc_raw;
-char msg[30];
+/* USER CODE BEGIN PV */
+
+// --- ADC ---
+uint16_t adc_raw = 0;
+
+// --- Promedio movil ---
+#define N_PROMEDIO 8
+uint32_t ventana[N_PROMEDIO] = {0};
+uint8_t  idx_ventana = 0;
+uint32_t suma_ventana = 0;
+uint16_t senal_suavizada = 0;
+
+// --- Deteccion de BPM ---
+// Ajustar UMBRAL segun la señal real del KY-038.
+#define UMBRAL_LATIDO 2200
+
+uint32_t ultimo_latido_ms = 0;
+uint16_t bpm = 0;
+uint8_t  en_pico = 0;
+
+// --- UART ---
+char msg[32];
+
+/* USER CODE END PV */
 
 int main(void)
 {
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
+
   HAL_Init();
-
   SystemClock_Config();
-
   MX_GPIO_Init();
   MX_ADC1_Init();
   MX_USART2_UART_Init();
 
+  /* USER CODE BEGIN 2 */
+  /* USER CODE END 2 */
+
   while (1)
-	{
-	  HAL_ADC_Start(&hadc1);
-		  if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK)
-		  {
-			  adc_raw = HAL_ADC_GetValue(&hadc1);
-			  int len = sprintf(msg, "ADC:%ld\r\n",adc_raw);
-			  HAL_UART_Transmit(&huart2, (uint8_t*)msg, len, 100);
-		  }
-	  HAL_Delay(10);
-	}
+  {
+    /* USER CODE BEGIN WHILE */
+
+    // 1. Leer ADC por polling
+    HAL_ADC_Start(&hadc1);
+    if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK)
+    {
+        adc_raw = (uint16_t)HAL_ADC_GetValue(&hadc1);
+
+        // 2. Promedio movil de N_PROMEDIO muestras
+        suma_ventana -= ventana[idx_ventana];
+        ventana[idx_ventana] = adc_raw;
+        suma_ventana += adc_raw;
+        idx_ventana = (idx_ventana + 1) % N_PROMEDIO;
+        senal_suavizada = (uint16_t)(suma_ventana / N_PROMEDIO);
+
+        // 3. Deteccion de latido sobre la señal suavizada
+        if (senal_suavizada > UMBRAL_LATIDO && !en_pico)
+        {
+            en_pico = 1;
+            uint32_t ahora = HAL_GetTick();
+
+            if (ultimo_latido_ms > 0)
+            {
+                uint32_t intervalo_ms = ahora - ultimo_latido_ms;
+
+                // Filtro fisiologico: 30 BPM (2000ms) a 200 BPM (300ms)
+                if (intervalo_ms >= 300 && intervalo_ms <= 2000)
+                {
+                    bpm = (uint16_t)(60000UL / intervalo_ms);
+                }
+            }
+            ultimo_latido_ms = ahora;
+        }
+        else if (senal_suavizada < UMBRAL_LATIDO)
+        {
+            en_pico = 0;
+        }
+
+        // 4. Enviar señal suavizada y BPM por UART
+        int len = sprintf(msg, "SEÑAL:%u BPM:%u\r\n", senal_suavizada, bpm);
+        HAL_UART_Transmit(&huart2, (uint8_t*)msg, len, 100);
+    }
+
+    HAL_Delay(10);
+    /* USER CODE END WHILE */
+
     /* USER CODE BEGIN 3 */
+  }
   /* USER CODE END 3 */
 }
 
@@ -44,14 +120,9 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -62,57 +133,32 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 2;
   RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) { Error_Handler(); }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                               | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) { Error_Handler(); }
 }
 
 /* USER CODE BEGIN 4 */
-
 /* USER CODE END 4 */
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
-  }
+  while (1) {}
   /* USER CODE END Error_Handler_Debug */
 }
+
 #ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
-#endif /* USE_FULL_ASSERT */
+#endif
